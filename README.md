08b4823bf6138125e9a8315c4a25cf525b60f0be
leonardo sandoval aya <leonardoaya1234@gmail.com>
2023-05-26 17:26:19 -0500

    Proyecto Integrador - Servidor lista de tareas

--- README.md
+++
@@ -1,5 +0,0 @@
-Qué sucedió al usar async y await?
-Al usar async y await,  el código es más sincrónica y legible. Al utilizar await antes de una función que devuelve una promesa, el flujo del programa se pausa hasta que la promesa se resuelva o se rechace. Esto evita el anidamiento excesivo de callbacks y permite un código más limpio y fácil de entender.
-
-¿Qué sucedió al usar el método then()?
-Al usar el método then() para manejar las promesas
--- index.js
+++ index.js
@@ -1,91 +1,18 @@
-const readline = require('readline');
+const express = require('express');
 
-const tasks = [];
+const app = express();
+const port = 8080;
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+const tasks = [
+  { id: 1, description: 'Hacer la compra', completed: false },
+  { id: 2, description: 'Limpiar la casa', completed: true },
+  { id: 3, description: 'Estudiar para el examen', completed: false }
+];
+
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
 });
 
-function addTask() {
-  return new Promise((resolve, reject) => {
-    rl.question('Ingrese la descripción de la tarea: ', (description) => {
-      const task = {
-        id: tasks.length + 1,
-        description: description,
-        completed: false
-      };
-      tasks.push(task);
-      console.log('Tarea agregada:');
-      console.log(task);
-      resolve();
-    });
-  });
-}
-
-function deleteTask() {
-  return new Promise((resolve, reject) => {
-    rl.question('Ingrese el ID de la tarea a eliminar: ', (taskId) => {
-      const index = tasks.findIndex(task => task.id === Number(taskId));
-      if (index !== -1) {
-        const deletedTask = tasks.splice(index, 1);
-        console.log('Tarea eliminada:');
-        console.log(deletedTask);
-      } else {
-        console.log('No se encontró una tarea con ese ID.');
-      }
-      resolve();
-    });
-  });
-}
-
-function completeTask() {
-  return new Promise((resolve, reject) => {
-    rl.question('Ingrese el ID de la tarea a marcar como completada: ', (taskId) => {
-      const task = tasks.find(task => task.id === Number(taskId));
-      if (task) {
-        task.completed = true;
-        console.log('Tarea marcada como completada:');
-        console.log(task);
-      } else {
-        console.log('No se encontró una tarea con ese ID.');
-      }
-      resolve();
-    });
-  });
-}
-
-function showMenu() {
-  console.log('------');
-  console.log('Seleccione una opción:');
-  console.log('1. Agregar tarea');
-  console.log('2. Eliminar tarea');
-  console.log('3. Marcar tarea como completada');
-  console.log('4. Salir');
-  rl.question('Opción seleccionada: ', async (option) => {
-    switch (option) {
-      case '1':
-        await addTask();
-        showMenu();
-        break;
-      case '2':
-        await deleteTask();
-        showMenu();
-        break;
-      case '3':
-        await completeTask();
-        showMenu();
-        break;
-      case '4':
-        rl.close();
-        break;
-      default:
-        console.log('Opción no válida. Intente nuevamente.');
-        showMenu();
-        break;
-    }
-  });
-}
-
-console.log('Bienvenido/a a la lista de tareas');
-showMenu();
+app.listen(port, () => {
+  console.log(`Servidor iniciado en http://localhost:${port}`);
+});
